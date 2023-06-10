18e214710d6685cdf48081f13af76ef7e74e94b1
552020 <stefanolombardo@posteo.de>
2023-06-10 12:17:09 +0200

    chore: create sign-up and sign-in routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,30 @@
-import { PrismaClient } from '@prisma/client'
-import cors from 'cors'
-import express from 'express'
+import { PrismaClient } from "@prisma/client";
+import cors from "cors";
+import express from "express";
 
-const prisma = new PrismaClient()
-const app = express()
+const prisma = new PrismaClient();
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/drafts', async (req, res) => {
+app.get("/drafts", async (req, res) => {
   const posts = await prisma.post.findMany({
     where: { published: false },
-    include: { author: true }
-  })
-  res.json(posts)
-})
+    include: { author: true },
+  });
+  res.json(posts);
+});
 
-app.get('/feed', async (req, res) => {
+app.get("/feed", async (req, res) => {
   const posts = await prisma.post.findMany({
     where: { published: true },
-    include: { author: true }
-  })
-  res.json(posts)
-})
+    include: { author: true },
+  });
+  res.json(posts);
+});
 
-app.get('/filterPosts', async (req, res) => {
+app.get("/filterPosts", async (req, res) => {
   const { searchString }: { searchString?: string } = req.query;
   const filteredPosts = await prisma.post.findMany({
     where: {
@@ -41,12 +41,12 @@ app.get('/filterPosts', async (req, res) => {
         },
       ],
     },
-  })
-  res.json(filteredPosts)
-})
+  });
+  res.json(filteredPosts);
+});
 
 app.post(`/post`, async (req, res) => {
-  const { title, content, authorEmail } = req.body
+  const { title, content, authorEmail } = req.body;
   const result = await prisma.post.create({
     data: {
       title,
@@ -54,51 +54,91 @@ app.post(`/post`, async (req, res) => {
       published: false,
       author: { connect: { email: authorEmail } },
     },
-  })
-  res.json(result)
-})
+  });
+  res.json(result);
+});
 
 app.delete(`/post/:id`, async (req, res) => {
-  const { id } = req.params
+  const { id } = req.params;
   const post = await prisma.post.delete({
     where: {
       id: Number(id),
     },
-  })
-  res.json(post)
-})
+  });
+  res.json(post);
+});
 
 app.get(`/post/:id`, async (req, res) => {
-  const { id } = req.params
+  const { id } = req.params;
   const post = await prisma.post.findUnique({
     where: {
       id: Number(id),
     },
-    include: { author: true }
-  })
-  res.json(post)
-})
+    include: { author: true },
+  });
+  res.json(post);
+});
 
-app.put('/publish/:id', async (req, res) => {
-  const { id } = req.params
+app.put("/publish/:id", async (req, res) => {
+  const { id } = req.params;
   const post = await prisma.post.update({
     where: { id: Number(id) },
     data: { published: true },
-  })
-  res.json(post)
-})
+  });
+  res.json(post);
+});
 
 app.post(`/user`, async (req, res) => {
   const result = await prisma.user.create({
     data: {
       ...req.body,
     },
-  })
-  res.json(result)
-})
+  });
+  res.json(result);
+});
 
-const server = app.listen(3001, () =>
-  console.log(
-    '🚀 Server ready at: http://localhost:3001',
-  ),
-)
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+app.post("/signup", async (req, res) => {
+  const { name, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  const token = jwt.sign({ id: user.id }, "your-secret-key");
+
+  res.json({ token });
+});
+
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    return res.status(400).json({ error: "No user found with this email." });
+  }
+
+  const passwordIsValid = bcrypt.compareSync(password, user.password);
+
+  if (!passwordIsValid) {
+    return res.status(400).json({ error: "Invalid password." });
+  }
+
+  const token = jwt.sign({ id: user.id }, "your-secret-key");
+
+  res.json({ token });
+});
+
+const server = app.listen(3001, () => console.log("🚀 Server ready at: http://localhost:3001"));
