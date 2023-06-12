e707075e3c1938f79d0b163729e34f2037466c8c
552020 <stefanolombardo@posteo.de>
2023-06-12 17:27:11 +0200

    chore: rewrite Post componeten in Tailwind

--- frontend/components/Post.tsx
+++ frontend/components/Post.tsx
@@ -1,32 +1,29 @@
-import React from 'react'
-import Router from 'next/router'
-import ReactMarkdown from 'react-markdown'
+import Router from "next/router";
+import ReactMarkdown from "react-markdown";
 
 export type PostProps = {
   id: number;
   title: string;
   author: {
     name: string;
-  }
+    email?: string;
+  };
   content: string;
   published: boolean;
-}
+};
 
-const Post: React.FC<{post: PostProps}> = ({ post }) => {
-  const authorName = post.author ? post.author.name : 'Unknown author'
+const Post: React.FC<{ post: PostProps }> = ({ post }) => {
+  const authorName = post.author ? post.author.name : "Unknown author";
   return (
-    <div onClick={() => Router.push('/p/[id]', `/p/${post.id}`)}>
-        <h2>{post.title}</h2>
-        <small>By {authorName}</small>
-        <ReactMarkdown children={post.content} />
-        <style jsx>{`
-          div {
-            color: inherit;
-            padding: 2rem;
-          }
-        `}</style>
+    <div
+      onClick={() => Router.push("/p/[id]", `/p/${post.id}`)}
+      className="p-4 my-2 bg-white rounded shadow cursor-pointer "
+    >
+      <h2 className="  text-2xl text-gray-700 bg-yellow-300 p-1 rounded-xl px-3">{post.title}</h2>
+      <p className="text-sm text-gray-500">By {authorName}</p>
+      <ReactMarkdown className="prose mt-4">{post.content}</ReactMarkdown>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
