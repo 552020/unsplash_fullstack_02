876d01e555d7a67339b8643a65d2a5bf9613e298
552020 <stefanolombardo@posteo.de>
2023-06-12 17:30:17 +0200

    chore: add edit button, rewrite [id].tsx in Tailwind

--- frontend/pages/p/[id].tsx
+++ frontend/pages/p/[id].tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { GetServerSideProps } from "next";
 import ReactMarkdown from "react-markdown";
 import Layout from "../../components/Layout";
@@ -8,13 +7,21 @@ import { PostProps } from "../../components/Post";
 async function publish(id: number): Promise<void> {
   await fetch(`${process.env.NEXT_PUBLIC_API_URL}/publish/${id}`, {
     method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    },
   });
-  await Router.push("/");
+  await Router.push("/${id}");
 }
 
 async function destroy(id: number): Promise<void> {
   await fetch(`${process.env.NEXT_PUBLIC_API_URL}/publish/${id}`, {
     method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    },
   });
   await Router.push("/");
 }
@@ -25,36 +32,42 @@ const Post: React.FC<PostProps> = (props) => {
     title = `${title} (Draft)`;
   }
 
+  const loggedInUser = typeof window !== "undefined" ? localStorage.getItem("email") : null;
+  const canEdit = loggedInUser && loggedInUser === props?.author?.email;
+
   return (
     <Layout>
-      <div>
-        <h2>{title}</h2>
-        <p>By {props?.author?.name || "Unknown author"}</p>
-        <ReactMarkdown children={props.content} />
-        {!props.published && <button onClick={() => publish(props.id)}>Publish</button>}
-        <button onClick={() => destroy(props.id)}>Delete</button>
+      <div className="border-2 border-gray-600 rounded-lg p-4">
+        <h2 className="text-2xl mb-2">{title}</h2>
+        <p>
+          By <span className="italic"> {props?.author?.name || "Unknown author"}</span>
+        </p>
+        <ReactMarkdown children={props.content} className="border-2 border-gray-600 rounded p-4 my-4 bg-white" />
+        {!props.published && (
+          <button
+            onClick={() => publish(props.id)}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Publish
+          </button>
+        )}
+        {canEdit && (
+          <button
+            onClick={() => destroy(props.id)}
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-4"
+          >
+            Delete
+          </button>
+        )}
+        {canEdit && (
+          <button
+            onClick={() => Router.push(`/edit/${props.id}`)}
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-4"
+          >
+            Edit
+          </button>
+        )}
       </div>
-      <style jsx>{`
-        .page {
-          background: white;
-          padding: 2rem;
-        }
-
-        .actions {
-          margin-top: 2rem;
-        }
-
-        button {
-          background: #ececec;
-          border: 0;
-          border-radius: 0.125rem;
-          padding: 1rem 2rem;
-        }
-
-        button + button {
-          margin-left: 1rem;
-        }
-      `}</style>
     </Layout>
   );
 };
