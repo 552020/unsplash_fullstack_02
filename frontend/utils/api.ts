d884323f9597379be175aee820598755fbf5f078
552020 <stefanolombardo@posteo.de>
2023-06-14 15:47:19 +0200

    chore: add func to get singles posts if authorized
    and update other functions in the api file

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -3,14 +3,24 @@ import { headersWithToken, getAuthToken } from "./auth";
 const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
 import { DraftProps } from "../components/Draft";
 
-export const createPost = async (
-  title: string,
-  content: string,
-  authorEmail: string,
-  token: string,
-  published: boolean
-) => {
+export const getPostByIdAuth = async (id: string) => {
   try {
+    const token = getAuthToken();
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    };
+    const res = await fetch(`${NEXT_PUBLIC_API_URL}/post/${id}`, { headers: headers });
+    const post = await res.json();
+    return post;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+export const createPost = async (title: string, content: string, authorEmail: string, published: boolean) => {
+  try {
+    const token = getAuthToken();
     const headers = {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
@@ -36,26 +46,34 @@ export const createPost = async (
   }
 };
 
-export const updatePost = async (post: DraftProps) => {
+export async function updatePost(id, title, content, authorEmail, published) {
   try {
-    const res = await fetch(`${NEXT_PUBLIC_API_URL}/post/${post.post.id}`, {
+    const token = getAuthToken();
+    const response = await fetch(`${NEXT_PUBLIC_API_URL}/post/${id}`, {
       method: "PUT",
-      headers: headersWithToken,
-      body: JSON.stringify(post),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ title, content, authorEmail, published }),
     });
-    const response = await res.json();
-    return response;
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(`Failed to update post: ${response.statusText}`);
+    }
   } catch (error) {
     console.log("error", error);
   }
-};
+}
 
-export const deletePost = async (post: DraftProps) => {
+// to delete a post we need only the post id
+export const deletePost = async (postId) => {
   try {
-    const res = await fetch(`${NEXT_PUBLIC_API_URL}/post/${post.post.id}`, {
+    const res = await fetch(`${NEXT_PUBLIC_API_URL}/post/${postId}`, {
       method: "DELETE",
       headers: headersWithToken,
-      body: JSON.stringify(post),
+      body: JSON.stringify(postId),
     });
     const response = await res.json();
     return response;
@@ -64,12 +82,28 @@ export const deletePost = async (post: DraftProps) => {
   }
 };
 
-export const publishPost = async (post: DraftProps) => {
+export const publishPost = async (postId) => {
   try {
-    const res = await fetch(`${NEXT_PUBLIC_API_URL}/publish/${post.post.id}`, {
+    const body = { published: true };
+    const res = await fetch(`${NEXT_PUBLIC_API_URL}/publish/${postId}`, {
       method: "PUT",
       headers: headersWithToken,
-      body: JSON.stringify(post),
+      body: JSON.stringify(body),
+    });
+    const response = await res.json();
+    return response;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+export const unpublishPost = async (postId) => {
+  try {
+    const body = { published: false };
+    const res = await fetch(`${NEXT_PUBLIC_API_URL}/publish/${postId}`, {
+      method: "PUT",
+      headers: headersWithToken,
+      body: JSON.stringify(false),
     });
     const response = await res.json();
     return response;
