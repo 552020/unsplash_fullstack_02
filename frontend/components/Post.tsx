a4af6a5d98d2c605370075273ebd27e84c2e2b50
552020 <stefanolombardo@posteo.de>
2023-06-14 11:39:07 +0200

    chore: change ref to new post folder in pages

--- frontend/components/Post.tsx
+++ frontend/components/Post.tsx
@@ -16,7 +16,7 @@ const Post: React.FC<{ post: PostProps }> = ({ post }) => {
   const authorName = post.author ? post.author.name : "Unknown author";
   return (
     <div
-      onClick={() => Router.push("/p/[id]", `/p/${post.id}`)}
+      onClick={() => Router.push("/posts/[id]", `/posts/${post.id}`)}
       className="p-4 my-2 bg-white rounded shadow cursor-pointer "
     >
       <h2 className="  text-2xl text-gray-700 bg-yellow-300 p-1 rounded-xl px-3">{post.title}</h2>
