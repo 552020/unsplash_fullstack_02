0aef24f29f2f069ce184242ea5cb114e94997a7d
552020 <stefanolombardo@posteo.de>
2023-06-15 12:45:06 +0200

    chore: add getAuthEmail util func

--- frontend/utils/auth.ts
+++ frontend/utils/auth.ts
@@ -5,6 +5,13 @@ export const getAuthToken = () => {
   return null;
 };
 
+export const getAuthEmail = () => {
+  if (typeof window !== "undefined") {
+    return localStorage.getItem("email");
+  }
+  return null;
+};
+
 export const headersWithToken = {
   "Content-Type": "application/json",
   Authorization: `Bearer ${getAuthToken()}`,
